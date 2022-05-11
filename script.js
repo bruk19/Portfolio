db7cd73eaafc9ef5f6bfd9ee7b57e2f0ae3ef0e0
bruk19 <brukteshome21@gmail.com>
2022-05-11 05:15:02 +0100

    add linters fix

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const menu = document.querySelector('#mob-mn')
-const menuLinks = document.querySelector('.nav-menu')
+const menu = document.querySelector('#mob-mn');
+const menuLinks = document.querySelector('.nav-menu');
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+menu.addEventListener('click', () => {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
 });
 
 document.querySelectorAll('.nav-lin').forEach((n) => n.addEventListener('click', () => {
-    menu.classList.remove('is-active');
-    menuLinks.classList.remove('active');
-  }));
+  menu.classList.remove('is-active');
+  menuLinks.classList.remove('active');
+}));
